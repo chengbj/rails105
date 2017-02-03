017b200767d6bace4d235d83102f11b7f65dca26
cheng df <cheng.010021@gmail.com>
2017-02-03 23:57:37 +0800

    there is doubt here

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :groups
   has_many :posts
+  # has_many :group_relationships
+  # has_many :participated_groups, :through => :group_relationships, :source => :group
+  has_many :group_relationships
+    has_many :participated, :through => :group_relationships, :source => :group
 end
