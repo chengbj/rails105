07bdc695896ff7085bfbe6493263279f670e8428
cheng df <cheng.010021@gmail.com>
2017-02-03 21:59:20 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def new
@@ -54,7 +55,7 @@ class GroupsController < ApplicationController
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
-  end 
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
