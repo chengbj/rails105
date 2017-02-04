cd38ea119c9f49ebd104d5382dd487a9268549d5
cheng df <cheng.010021@gmail.com>
2017-02-04 20:54:11 +0800

    still in doubt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202134144) do
+ActiveRecord::Schema.define(version: 20170204112325) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ puts "this is a seed file"
+
+ create_account = User.create([email: 'cdf@newborn.com', password: '111111', password_confirmation: '111111'])
+ puts "account created"
+
+ created_groups= for i in 1..10 do
+   Group.create!([title: "board no.#{i}", description: "this is board #{i}", user_id: '1'])
+ end
+ puts "10 board created"
+
+ created_posts = for j in 1..10 do
+   Post.create!([content: "post no.#{j}",user_id: '1', group_id: '1'])
+ end
+ puts "10 posts created"
